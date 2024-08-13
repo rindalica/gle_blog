56d72f0ff503d5a01f48dfc417435aa30f5c8cca
정지민 <rindalica@gmail.com>
2024-08-13 10:37:52 +0900

    Fix Header & layout style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className='flex justify-between items-center p-[10px] bg-pink-50'>
+    <header className='sticky top-0 flex justify-between items-center px-[20px] py-[10px] bg-black text-white font-bold'>
       <Link href='/'>
-        <h1 className='text-[30px]'>GLE BLOG</h1>
+        <h1 className='text-[30px]'>rindalica&apos;s blog</h1>
       </Link>
       <nav className='flex gap-[10px]'>
         <Link href='/'>HOME</Link>
