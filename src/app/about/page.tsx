cb5619741283509309b78da6325cf336709b1fbf
정지민 <rindalica@gmail.com>
2024-08-13 10:38:33 +0900

    Update About page 내용추가

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export default function AboutPage() {
-  return <div>About</div>;
+  return (
+    <div className='py-[20px]'>
+      <h1 className='text-[48px] font-bold leading-[60px]'>
+        안녕하세요. <br />
+        성실함의 힘을 믿는 개발자 <br />
+        정지민입니다.
+      </h1>
+    </div>
+  );
 }
