a633b5d4125e69e5d5883c38e784140f928c7e5f
정지민 <rindalica@gmail.com>
2024-11-19 09:33:41 +0900

    Fix contact page 수정.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function ContactPage() {
-  return <div>Contact</div>;
+  return (
+    <div className='flex flex-col justify-center items-center h-full gap-[10px] text-2xl'>
+      <div>📧 rindalica@gmail.com </div>
+      <div>📞 010-8723-5394 </div>
+    </div>
+  );
 }
