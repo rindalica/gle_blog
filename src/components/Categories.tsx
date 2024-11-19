74e0d464f221e77ccfdae9784d02a8959367b548
정지민 <rindalica@gmail.com>
2024-11-19 10:36:56 +0900

    Fix category style

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -8,12 +8,13 @@ type Props = {
 
 export default function Categories({ categories, selected, onClick }: Props) {
   return (
-    <div>
+    <div className='flex justify-between p-[10px] mb-[10px] border-b-4'>
       <h2>Category</h2>
-      <ul>
+      <ul className='flex gap-[10px]'>
         {categories.map((category) => (
           <li
             key={category}
+            className='pr-[5px] border-r-2'
             onClick={() => {
               onClick(category);
             }}
