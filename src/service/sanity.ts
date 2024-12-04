0a6c90d4280bad8d85034e3ddfff296e48cb1813
정지민 <rindalica@gmail.com>
2024-12-04 15:37:34 +0900

    Fix sanity 환경 변수 설정.

--- src/service/sanity.ts
+++ src/service/sanity.ts
@@ -1,10 +1,9 @@
 import { createClient } from '@sanity/client';
 export const client = createClient({
   //   projectId: process.env.SANITY_PROJECT_ID,
-  projectId: 'vsjp4pqu',
-  dataset: 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   useCdn: false, // set to `false` to bypass the edge cache
   apiVersion: '2024-12-02', // use current date (YYYY-MM-DD) to target the latest API version
-  token:
-    'skqAye2Dy50TqiZyFtQheKlq6dTj9Vyy8EiUmE8AGkDJ48juwHXPFXHY4zyDY0gI2pT6UMyOHBkjwVuLeUCLCFZ2htEaj583SDhpTl3sPurzIpNLCX9kxVBXaPisCShk46eafjDaTsktkwJ3dSHuGcVxWbhgR9aBvENOfKwMrmHHbMRmXSnJ', // Needed for certain operations like updating content or accessing previewDrafts perspective
+  token: process.env.NEXT_PUBLIC_SANITY_SECRET_TOKEN,
 });
